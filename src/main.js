23c8e80e127bde9cfc04523d5fc01e89f507ea41
EgamixYT <55224974+EgamixYT@users.noreply.github.com>
2021-12-07 23:02:57 +0200

    fixes

--- src/main.js
+++ src/main.js
@@ -4,24 +4,10 @@ import router from './router/index.js'
 import './assets/index.css' 
 import global from "./global"
 import store from './store'
-import { initializeApp } from "firebase/app";
-import { required, email } from '@vuelidate/validators'
 
 
-initializeApp({
-    apiKey: "AIzaSyBSRT3HkmD5y8XMOEkF5Tq_6v0ekeAPx4Y",
 
-    authDomain: "english-words-d4611.firebaseapp.com",
-  
-    projectId: "english-words-d4611",
-  
-    storageBucket: "english-words-d4611.appspot.com",
-  
-    messagingSenderId: "901168815277",
-  
-    appId: "1:901168815277:web:fd9d6cf399d4954d237a24"
-  
-})
+
 const app = createApp(App).use(router).use(store)
 global(app)
 app.mount("#app")
